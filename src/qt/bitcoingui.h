7ad275d05c75bf8c53bacf1651f35e076c424c13
Kyle Gingrich <pwnallnoobs@gmail.com>
2014-05-13 20:53:06 -0400

    Add Staking Icon 	modified:   src/bitcoinrpc.h 	modified:   src/kernel.cpp 	modified:   src/kernel.h 	modified:   src/main.cpp 	modified:   src/qt/bitcoin.qrc 	modified:   src/qt/bitcoingui.cpp 	modified:   src/qt/bitcoingui.h 	modified:   src/rpcblockchain.cpp 	modified:   src/wallet.cpp 	modified:   src/wallet.h

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -4,6 +4,8 @@
 #include <QMainWindow>
 #include <QSystemTrayIcon>
 
+#include "util.h" // for uint64
+
 class TransactionTableModel;
 class ClientModel;
 class WalletModel;
@@ -67,6 +69,7 @@ private:
     SignVerifyMessageDialog *signVerifyMessageDialog;
 
     QLabel *labelEncryptionIcon;
+    QLabel *labelMintingIcon;
     QLabel *labelConnectionsIcon;
     QLabel *labelBlocksIcon;
     QLabel *progressBarLabel;
@@ -88,6 +91,7 @@ private:
     QAction *encryptWalletAction;
     QAction *backupWalletAction;
     QAction *changePassphraseAction;
+    QAction *lockWalletToggleAction;
     QAction *aboutQtAction;
     QAction *openRPCConsoleAction;
 
@@ -97,6 +101,10 @@ private:
     RPCConsole *rpcConsole;
 
     QMovie *syncIconMovie;
+
+    uint64 nMinMax;
+    uint64 nWeight;
+    uint64 nNetworkWeight;
 
     /** Create the main UI actions. */
     void createActions();
@@ -174,6 +182,11 @@ private slots:
     void showNormalIfMinimized(bool fToggleHidden = false);
     /** simply calls showNormalIfMinimized(true) for use in SLOT() macro */
     void toggleHidden();
+
+    /** Update info about minting */
+    void updateMintingIcon();
+    /** Update minting weight info */
+    void updateMintingWeights();
 };
 
 #endif
